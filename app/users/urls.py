2ad49612e06d8b0e7a6c3e08333e8fb81d6c370d
dmeteo <pesh.dmetro@gmail.com>
2024-06-19 05:43:06 +0500

    reg auth logout profile + projects app

---
+++ app/users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from app.users import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('profile/', views.profile, name='profile'),
+    path('logout/', views.logout, name='logout'),
+]
